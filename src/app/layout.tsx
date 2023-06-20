ae642eb20f5fc0386b40fb5943c6067a4792ea5e
Lukass9 <l.pisarek@o2.pl>
2023-06-20 08:17:17 +0200

    create basice struct

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Lama Dev TypeScript Tutorial",
+  description: "Learn TypeScript",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <div className='container'>
+          <Navbar />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
