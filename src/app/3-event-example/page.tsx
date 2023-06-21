37f6816d0f91093b74f63f11960b22aed6c73777
Lukass9 <l.pisarek@o2.pl>
2023-06-21 07:23:13 +0200

    react event

--- src/app/3-event-example/page.tsx
+++ src/app/3-event-example/page.tsx
@@ -2,31 +2,31 @@
 import React from "react";
 
 const EventExample = () => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-  };
-
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     console.log("Searched!");
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
     e.preventDefault();
-    console.log(`Post ${id} has been deleted!`);
+    console.log(`Post ${id} has been deleted! `);
   };
 
   return (
-    <div className="eventExample">
+    <div className='eventExample'>
       <form>
         <input
-          type="text"
-          placeholder="Search for anything..."
+          type='text'
+          placeholder='Search for anything...'
           onChange={handleChange}
         />
         <button onClick={handleClick}>Search</button>
       </form>
-      <form className="post">
+      <form className='post'>
         <h1>Lorem ipsum dolor sit amet consectetur 1</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam
@@ -36,7 +36,7 @@ const EventExample = () => {
         </p>
         <button onClick={(e) => handleDelete(e, 1)}>Delete</button>
       </form>
-      <form className="post">
+      <form className='post'>
         <h1>Lorem ipsum dolor sit amet consectetur 2</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ullam
